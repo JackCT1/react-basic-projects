007c3b5b8fe41db9bae65f7f614610f7ce4540fa
JackCT1 <jackthomas933@gmail.com>
2023-06-09 18:19:34 +0100

    loading page

--- 02-tours-list/tours-list/src/App.jsx
+++ 02-tours-list/tours-list/src/App.jsx
@@ -1,4 +1,20 @@
+import { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {};
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
   return (
     <>
       <h1>Second React App!</h1>
