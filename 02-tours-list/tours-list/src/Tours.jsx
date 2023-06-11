1727bb205f1a3fc6e51c8b111a7e8c7a271f7e1f
JackCT1 <jackthomas933@gmail.com>
2023-06-11 16:02:20 +0100

    returning tour components

--- 02-tours-list/tours-list/src/Tours.jsx
+++ 02-tours-list/tours-list/src/Tours.jsx
@@ -1,7 +1,19 @@
 import Tour from "./Tour";
 
-const Tours = () => {
-  return <h1>Tours Component</h1>;
+const Tours = ({ tours }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>Our Tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour}></Tour>;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
