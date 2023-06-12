317e8c7d114d40b1ae6ca6e5293897d31f82e510
JackCT1 <jackthomas933@gmail.com>
2023-06-12 22:02:28 +0100

    title renamed and form elements added

--- 00-todo-list/src/App.jsx
+++ 00-todo-list/src/App.jsx
@@ -1,5 +1,19 @@
 import { useState } from "react";
+import "./styles.css";
 
-function App() {}
+function App() {
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>;
+    </>
+  );
+}
 
 export default App;
