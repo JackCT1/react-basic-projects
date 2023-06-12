ca96c5f5b0068253f85a874543d34aa84833915e
JackCT1 <jackthomas933@gmail.com>
2023-06-12 18:12:41 +0100

    remove tour button working

--- 02-tours-list/tours-list/src/Tour.jsx
+++ 02-tours-list/tours-list/src/Tour.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const Tour = ({ id, image, info, name, price }) => {
+const Tour = ({ id, image, info, name, price, removeTour }) => {
   const [readMore, setReadMore] = useState(false);
   return (
     <article className="single-tour">
@@ -20,7 +20,9 @@ const Tour = ({ id, image, info, name, price }) => {
             {readMore ? "show less" : "read more"}
           </button>
         </p>
-        <button className="delete-btn">not interested</button>
+        <button className="delete-btn" onClick={() => removeTour(id)}>
+          not interested
+        </button>
       </footer>
     </article>
   );
