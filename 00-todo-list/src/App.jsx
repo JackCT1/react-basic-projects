16863ad6dc98d22297dd1f29821237befce98bae
JackCT1 <jackthomas933@gmail.com>
2023-06-13 20:57:56 +0100

    delete todo event listener

--- 00-todo-list/src/App.jsx
+++ 00-todo-list/src/App.jsx
@@ -59,7 +59,12 @@ function App() {
                 />
                 {todo.title}
               </label>
-              <button className="btn btn-danger">Delete</button>
+              <button
+                onClick={() => deleteTodo(todo.id)}
+                className="btn btn-danger"
+              >
+                Delete
+              </button>
             </li>
           );
         })}
